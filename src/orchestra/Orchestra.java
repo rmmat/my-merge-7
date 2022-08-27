e2e79de5386df7f8752f9744a224de41d3050b61
Richard Matsumoto <richard.matsumoto@gmail.com>
2022-08-27 14:52:55 -0400

    add Section

--- src/orchestra/Orchestra.java
+++ src/orchestra/Orchestra.java
@@ -1,8 +1,20 @@
 package orchestra;
 
+import orchestra.sections.Section;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Orchestra {
+    private List<Section> sections;
+
+    private Orchestra() {
+        this.sections = new ArrayList<>();
+    }
+
     private void print() {
         System.out.println("=== Orchestra Members ===");
+        this.sections.forEach(Section::print);
     }
 
     public static void main(String[] args) {
---
+++ src/orchestra/sections/Section.java
@@ -0,0 +1,25 @@
+package orchestra.sections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Section {
+    private String name;
+    private List<String> instruments;
+
+    public Section(String name) {
+        this.name = name;
+        this.instruments = new ArrayList<>();
+    }
+
+    protected void addInstrument(String instrument) {
+        this.instruments.add(instrument);
+    }
+
+    public void print() {
+        System.out.println(this.name + ":");
+        this.instruments.forEach(instrument -> {
+            System.out.println('\t' + instrument);
+        });
+    }
+}
