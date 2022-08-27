af740715ec02c58b10e19e733449235d915c70fe
Richard Matsumoto <richard.matsumoto@gmail.com>
2022-08-27 14:44:57 -0400

    add Orchestra

---
+++ src/orchestra/Orchestra.java
@@ -0,0 +1,13 @@
+package orchestra;
+
+public class Orchestra {
+    private void print() {
+        System.out.println("=== Orchestra Members ===");
+    }
+
+    public static void main(String[] args) {
+        Orchestra orchestra = new Orchestra();
+
+        orchestra.print();
+    }
+}
